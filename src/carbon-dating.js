dfc2c9e36666986b984723359c56269e49e3ceb1
rockmonolit <rockmonolit@mail.ru>
2022-10-15 14:53:20 +0300

    feat: solve carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,22 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const k = 0.693 / HALF_LIFE_PERIOD; // k = 0.693 / (t 1/2)
+  const n = +sampleActivity;
+  const t = Math.log(MODERN_ACTIVITY / n) / k; // t = ln(N0/N)/k
+
+  if (!sampleActivity ||
+      typeof sampleActivity !== "string" ||
+      isNaN(n) ||
+      sampleActivity === null ||
+      sampleActivity === undefined ||
+      sampleActivity > MODERN_ACTIVITY||
+      sampleActivity <= 0) {
+    return false;
+  } else {
+    return Math.ceil(t);
+  }
 }
 
 module.exports = {
